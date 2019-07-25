906109dee03d7e274d8534bd5e67b8015b5fbf83
Marco Wessel <marco.wessel@pakoenig.de>
2019-07-25 16:03:07 +0200

    first version

---
+++ Game.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Game
+    {
+        private bool _gameOver;
+        private bool _playerGuessedWord;
+
+        private string _wordToGuess;
+        private string _wordToGuessWithUnderscores;
+
+        private int _guessErrorsMaximum;
+        private int _playerGuessErrorCounter;
+
+        private readonly List<char> _alreadyGuessedLetters;
+        private readonly HangmanDrawer _hangmanDrawer;
+
+        public Game()
+        {
+            // set default values
+            this._gameOver = false;
+            this._playerGuessedWord = false;
+
+            this._wordToGuess = "";
+            this._wordToGuessWithUnderscores = "";
+
+            this._guessErrorsMaximum = 0;
+            this._playerGuessErrorCounter = 0;
+
+            this._alreadyGuessedLetters = new List<char>();
+            this._hangmanDrawer = new HangmanDrawer();
+        }
+
+        public void Start()
+        {
+            Console.Clear();
+            Welcome();
+            SetGameSettingsFromPlayer();
+            GuessLoop();
+        }
+
+        private void Welcome()
+        {
+            Console.WriteLine("WELCOME TO HANGMAN!");
+            HangmanDrawer.Draw();
+        }
+
+        private void SetGameSettingsFromPlayer()
+        {
+            this._wordToGuess = GetWordToGuessFromPlayer();
+            this._wordToGuessWithUnderscores = ConvertWordToUnderscores(this._wordToGuess);
+            this._guessErrorsMaximum = GetGuessesMaximumFromPlayer();
+        }
+
+        private void GuessLoop()
+        {
+            while (!this._gameOver)
+            {
+                Console.Clear();
+                PrintGuessOrLetterHeadinfo();
+                ExecuteGuessChoice(GetPlayerGuessChoice());
+                CheckGameOverConditions();
+                Console.Write("\n\n\nPRESS ANY KEY TO CONTINUE");
+                Console.ReadKey();
+            }
+        }
+
+        private void PrintGuessOrLetterHeadinfo()
+        {
+            this._hangmanDrawer.DrawInDependenceOfMaxAndCurrentCount(this._guessErrorsMaximum, this._playerGuessErrorCounter);
+            PrintWordWithSpaces(this._wordToGuessWithUnderscores, 1);
+            Console.Write($"\n[{this._playerGuessErrorCounter + 1} / {this._guessErrorsMaximum}] Choose guess or letter (1 or 2):\n");
+            Console.WriteLine("----------------------------------------");
+        }
+
+        private void ExecuteGuessChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    WordGuess();
+                    break;
+                case 2:
+                    LetterGuess();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void WordGuess()
+        {
+            Console.Write("\nInput your guess: ");
+            string playerWordGuess = Console.ReadLine().ToUpper();
+
+            if (playerWordGuess == this._wordToGuess)
+            {
+                _playerGuessedWord = true;
+            }
+            else
+            {
+                Console.WriteLine("You guessed wrong...");
+                _playerGuessErrorCounter++;
+            }
+        }
+
+        private void LetterGuess()
+        {
+            Console.Write("\nInput your letter: ");
+            PrintAlreadyGuessedLetters();
+
+            if (CheckIfWordContainsLetterGuess(GetPlayerLetterGuess()))
+            {
+                Console.WriteLine("Word contains your letter!\n");
+            }
+            else
+            {
+                Console.WriteLine("Word does not contain letter...\n");
+                _playerGuessErrorCounter++;
+            }
+        }
+
+        private void PrintAlreadyGuessedLetters()
+        {
+            if (_alreadyGuessedLetters.Count != 0)
+            {
+                Console.WriteLine("\nAlready Guessed Letters(" +
+                    string.Join(",", _alreadyGuessedLetters.ToArray())
+                    + ")"
+                );
+            }
+        }
+
+        private bool CheckIfWordContainsLetterGuess(char playerLetterGuess)
+        {
+            bool letterInString = false;
+
+            for (int index = 0; index < this._wordToGuess.Length; index++)
+            {
+                if (this._wordToGuess[index] == playerLetterGuess)
+                {
+                    StringBuilder sb = new StringBuilder(this._wordToGuessWithUnderscores);
+                    sb[index] = playerLetterGuess;
+                    this._wordToGuessWithUnderscores = sb.ToString();
+                    letterInString = true;
+                }
+            }
+
+            return letterInString;
+        }
+
+        private char GetPlayerLetterGuess()
+        {
+            bool playerInputIsValid = false;
+            char playerLetterGuess = ' ';
+            string tryAgain = " Try again.\n";
+
+            while (!playerInputIsValid)
+            {
+                if (!(Char.TryParse(Console.ReadLine().ToUpper(), out playerLetterGuess)))
+                {
+                    Console.Write("Input needs to be a character!" + tryAgain);
+                    continue;
+                }
+                else if (!Char.IsLetter(playerLetterGuess))
+                {
+                    Console.Write("Input needs to be a letter!" + tryAgain);
+                    continue;
+                }
+                else if (CheckIfLetterWasAlreadyGuessed(playerLetterGuess))
+                {
+                    Console.WriteLine("This letter was already guessed!" + tryAgain);
+                    continue;
+                }
+
+                playerInputIsValid = true;
+            }
+
+            return playerLetterGuess;
+        }
+
+        private int GetPlayerGuessChoice()
+        {
+            bool playerInputIsValid = false;
+            int guessOrLetter = 0;
+            string tryAgain = " Try again.\n";
+
+            while (!playerInputIsValid)
+            {
+                if (!(int.TryParse(Console.ReadLine(), out guessOrLetter)))
+                {
+                    Console.Write("Input needs to be a digit!" + tryAgain);
+                    continue;
+                }
+                else if (!(guessOrLetter == 1 || guessOrLetter == 2))
+                {
+                    Console.Write("Input must be 1 or 2!" + tryAgain);
+                    continue;
+                }
+
+                playerInputIsValid = true;
+            }
+
+            return guessOrLetter;
+        }
+
+        private void CheckGameOverConditions()
+        {
+            // player has no guesses left
+            if (this._playerGuessErrorCounter == this._guessErrorsMaximum)
+            {
+                Console.WriteLine("You LOSE word was: " + this._wordToGuess);
+                this._gameOver = true;
+            }
+            // player guessed the word
+            else if (this._playerGuessedWord)
+            {
+                PrintWinMessage();
+                this._gameOver = true;
+            }
+            // player found out all letters
+            else if (this._wordToGuess == this._wordToGuessWithUnderscores)
+            {
+                PrintWinMessage();
+                this._gameOver = true;
+            }
+        }
+
+        private void PrintWinMessage()
+        {
+            Console.WriteLine("You guessed right! EASY WIN!\n");
+            Console.WriteLine("Word was: " + this._wordToGuess);
+        }
+
+        private bool CheckIfLetterWasAlreadyGuessed(char playerLetterGuess)
+        {
+            bool letterAlreadyGuessed = false;
+
+            foreach (char alreadyGuessedletter in this._alreadyGuessedLetters)
+            {
+                if (playerLetterGuess == alreadyGuessedletter)
+                {
+                    letterAlreadyGuessed = true;
+                }
+            }
+
+            if (letterAlreadyGuessed == false)
+            {
+                this._alreadyGuessedLetters.Add(playerLetterGuess);
+            }
+
+            return letterAlreadyGuessed;
+        }
+
+        private void End()
+        {
+
+        }
+
+        private int GetGuessesMaximumFromPlayer()
+        {
+            bool playerInputIsValid = false;
+            int guessesMaximum = 0;
+
+            Console.Write("Type amount of guesses (Standard 10): ");
+
+            while (!playerInputIsValid)
+            {
+                if (!(int.TryParse(Console.ReadLine(), out guessesMaximum)))
+                {
+                    Console.WriteLine("Input needs to be a number, try again: ");
+                    continue;
+                }
+                else if (!(guessesMaximum > 0))
+                {
+                    Console.WriteLine("Input number needs to be possitiv, try again: ");
+                    continue;
+                }
+                else if (!(guessesMaximum >= 10))
+                {
+                    Console.WriteLine("Input number must be 10 or bigger, try again: ");
+                    continue;
+                }
+
+                playerInputIsValid = true;
+            }
+
+            return guessesMaximum;
+        }
+
+        private string GetWordToGuessFromPlayer()
+        {
+            bool wordToGuessIsValid = false;
+            string wordToGuess = "";
+
+            Console.Write("Write word to guess: ");
+
+            while (!wordToGuessIsValid) 
+            {
+                wordToGuess = Console.ReadLine();
+
+                if (wordToGuess.All(Char.IsLetter))
+                {
+                    wordToGuessIsValid = true;
+                }
+                else
+                {
+                    Console.Write("Word is not allowed to have numbers and special chars, try again: ");
+                    wordToGuessIsValid = false;
+                }
+            }
+
+            return wordToGuess.ToUpper();
+        }
+
+        private string ConvertWordToUnderscores(string word)
+        {
+            string underscores = "";
+
+            for (int counter = 0; counter < word.Length; counter++) { underscores += "_"; }
+
+            return underscores;
+        }
+
+        private void PrintWordWithSpaces(string word, int amountSpaces = 1)
+        {
+            string wordWithSpaces = "";
+            string letterSpace = new String(' ', amountSpaces);
+
+            for (int index = 0; index < word.Length; index++)
+            {
+                wordWithSpaces += word[index] + letterSpace;
+            }
+
+            Console.WriteLine(wordWithSpaces.Trim());
+        }
+    }
+}
---
+++ HangmanDrawer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    sealed class HangmanDrawer
+    {
+        private int _currentStage = 0;
+        private readonly static string[] _stages = {
+            "               \n" +
+            "               \n" +
+            "               \n" +
+            "               \n" +
+            "               \n" +
+            "               \n" +
+            "                 ",
+
+            "               \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|                ",
+
+            " _________     \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|                ",
+
+            " _________     \n" +
+            "|         |    \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|                ",
+
+            " _________     \n" +
+            "|         |    \n" +
+            "|         0    \n" +
+            "|              \n" +
+            "|              \n" +
+            "|              \n" +
+            "|                ",
+
+            " _________     \n" +
+            "|         |    \n" +
+            "|         0    \n" +
+            "|         |    \n" +
+            "|              \n" +
+            "|              \n" +
+            "|                ",
+
+            " _________     \n" +
+            "|         |    \n" +
+            "|         0    \n" +
+            "|        /|    \n" +
+            "|              \n" +
+            "|              \n" +
+            "|                ",
+
+            " _________     \n" +
+            "|         |    \n" +
+            "|         0    \n" +
+            "|        /|\\  \n" +
+            "|              \n" +
+            "|              \n" +
+            "|                ",
+
+            " _________     \n" +
+            "|         |    \n" +
+            "|         0    \n" +
+            "|        /|\\  \n" +
+            "|        /     \n" +
+            "|              \n" +
+            "|                ",
+
+            " _________     \n" +
+            "|         |    \n" +
+            "|         0    \n" +
+            "|        /|\\  \n" +
+            "|        / \\  \n" +
+            "|              \n" +
+            "|                "};
+
+        public HangmanDrawer()
+        {
+            this._currentStage = 0;
+        }
+
+        public void DrawInDependenceOfMaxAndCurrentCount(int maxCount, int currentCount)
+        {
+            int amountStages = _stages.Length;
+            int stepsToNextStage = Convert.ToInt32(Math.Floor(Convert.ToDouble(maxCount / amountStages)));
+
+            if (currentCount == 0)
+            {
+                this._currentStage = 0;
+            }
+            else if (currentCount == maxCount)
+            {
+                this._currentStage = amountStages;
+            }
+            else if (currentCount % stepsToNextStage == 0)
+            {
+                this._currentStage = (currentCount / stepsToNextStage);
+            }
+
+            Draw(this._currentStage);
+        }
+
+        public static void Draw(int stageNumber = 9)
+        {
+            for (int index = 0; index < _stages.Length; index++)
+            {
+                if (index == stageNumber)
+                {
+                    Console.WriteLine(_stages[index]);
+                    break;
+                }
+            }
+        }
+    }
+}
